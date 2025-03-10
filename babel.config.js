372bf76cb448ad5fb4a318d3b8111abf0c5d760d
Rindah Ngwanyam <ngwanyam@gmail.com>
2025-03-10 03:33:31 +0100

    Add Metro and Babel configurations for better module resolution

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,26 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+          extensions: [
+            '.ios.ts',
+            '.android.ts',
+            '.ts',
+            '.ios.tsx',
+            '.android.tsx',
+            '.tsx',
+            '.jsx',
+            '.js',
+            '.json',
+          ],
+        },
+      ],
     ],
   };
 }; 
